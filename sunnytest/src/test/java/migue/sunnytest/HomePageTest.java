925a4a9cf2fe5c0170973360ec4fabd78c85e789
miguelbravo09 <miguelbravo090596@gmail.com>
2024-10-23 09:59:33 -0700

    Additional test class and switched to WebDriverManager

--- sunnytest/src/test/java/migue/sunnytest/HomePageTest.java
+++ sunnytest/src/test/java/migue/sunnytest/HomePageTest.java
@@ -9,16 +9,20 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class HomePageTest {
 
 	WebDriver driver;
 	
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver-win64/chromedriver.exe");
-        
-        driver = new ChromeDriver();
-        
+        //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver-win64/chromedriver.exe");
+                
+    	WebDriverManager.chromedriver().setup();
+    	
+    	driver = new ChromeDriver();
+    	
         driver.get("https://the-internet.herokuapp.com/");
     }
 
