b902a1be8a21401ef4196313d76749580c690418
miguelbravo09 <miguelbravo090596@gmail.com>
2024-10-28 13:09:37 -0700

    Headless option added

--- sunnytest/src/test/java/migue/sunnytest/drivers/WebDriverFactory.java
+++ sunnytest/src/test/java/migue/sunnytest/drivers/WebDriverFactory.java
@@ -2,29 +2,51 @@ package migue.sunnytest.drivers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebDriverFactory {
 	public static WebDriver getDriver(String browser) {
         WebDriver driver;
+        
+        boolean headless = true;
 
         switch (browser.toLowerCase()) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();  // WebDriverManager setup
-                driver = new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if(headless) {
+                	chromeOptions.addArguments("--headless");
+                    chromeOptions.addArguments("--disable-gpu");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                }
+                driver = new ChromeDriver(chromeOptions);
                 break;
 
             case "firefox":
                 WebDriverManager.firefoxdriver().setup(); // WebDriverManager setup
-                driver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if(headless) {
+                	firefoxOptions.addArguments("-headless");
+                }
+                driver = new FirefoxDriver(firefoxOptions);
                 break;
 
             case "edge":
                 WebDriverManager.edgedriver().setup();    // WebDriverManager setup
-                driver = new EdgeDriver();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                if(headless) {
+                	edgeOptions.addArguments("headless");
+                	edgeOptions.addArguments("disable-gpu");
+                    edgeOptions.addArguments("window-size=1920,1080");
+                	
+                }
+                driver = new EdgeDriver(edgeOptions);
                 break;
 
             default:
